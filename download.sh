3960ef6f32c4676f5aec8bb283a319b38c45c0fa
Akash Chikara <akashchikara1998@gmail.com>
2025-11-09 01:23:04 +0530

    first commit

---
+++ download.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+curl -s https://erp.subharti.org/LoginCss/css/bootstrap.css -o LoginCss/css/bootstrap.css
+curl -s https://erp.subharti.org/LoginCss/css/login.css -o LoginCss/css/login.css
+curl -s https://erp.subharti.org/LoginCss/css/bttn.min.css -o LoginCss/css/bttn.min.css
+curl -s https://erp.subharti.org/LoginCss/css/slant.css -o LoginCss/css/slant.css
+curl -s https://erp.subharti.org/LoginCss/css/animate-custom.css -o LoginCss/css/animate-custom.css
+curl -s https://erp.subharti.org/LoginCss/img/subhart-png.png -o LoginCss/img/subhart-png.png
+curl -s https://erp.subharti.org/LoginCss/js/jquery-1.9.1.min.js -o LoginCss/js/jquery-1.9.1.min.js
+curl -s https://erp.subharti.org/LoginCss/js/bootstrap.min.js -o LoginCss/js/bootstrap.min.js
+curl -s https://erp.subharti.org/LoginCss/js/placeholder-shim.min.js -o LoginCss/js/placeholder-shim.min.js
+curl -s https://erp.subharti.org/LoginCss/js/custom.js -o LoginCss/js/custom.js
+curl -s https://erp.subharti.org/slider/libs/jquery/jquery.js -o slider/libs/jquery/jquery.js
+curl -s https://erp.subharti.org/slider/src/jquery.backstretch.js -o slider/src/jquery.backstretch.js
+curl -s https://erp.subharti.org/slider/d1.jpg -o slider/d1.jpg
+curl -s https://erp.subharti.org/slider/d2.jpg -o slider/d2.jpg
+curl -s https://erp.subharti.org/slider/d3.jpg -o slider/d3.jpg
+curl -s https://erp.subharti.org/slider/d4.jpg -o slider/d4.jpg
+curl -s https://erp.subharti.org/slider/d5.jpg -o slider/d5.jpg
